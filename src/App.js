eea8398ee6355a9a06349f0ed2a4bde587f7e341
Sofiqul Islam <shiplo.msi2013@gmail.com>
2025-12-28 11:07:19 +0600

    Styling JSX element in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
 
 function App() {
   return (
     <div>
       <Header />
+      <Nav />
+      <Promo />
+      <Footer />
     </div>
   );
 }
